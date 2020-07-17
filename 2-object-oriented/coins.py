b580000b7209b8fd551be5c52dc068353ed1ddbf
Thomas Whaley <thomas.whaley@metaswitch.com>
2020-07-17 20:17:20 +1200

    Solutions

--- 2-object-oriented/coins.py
+++ 2-object-oriented/coins.py
@@ -2,13 +2,17 @@ import random
 
 
 # 1 - Let's create a class called OurClass, with a property named x.
+class OurClass:
+    x = 7
 
 
 # Create a variable called "our_object" that is an instance of this class.
-
+our_object = OurClass()
 # Access and print the property of our_object.
-
+print(our_object.x)
 # Update and print our_object's property.
+our_object.x = 0
+print(our_object.x)
 
 
 # 2 - The __init__() function.
@@ -20,12 +24,39 @@ import random
 # value, which defaults to 1.0.
 # Let's also define a property describing which side of the coin is face-up, defining a
 # class function to "flip" the coin.
+class Coin:
+
+    # Notice that the first parameter is self. This refers to the current instance
+    # of an object, and is (almost) always the first parameter of functions in classes.
+    def __init__(self, value=1.0):
+        self.value = value
+        self.side_facing_up = "heads"
+
+    # Again, the first (and only) argument is self, referring to the current object instance.
+    def flip(self):
+        # Let's get Python to generate a random integer between 0 and 1,
+        # using that to choose head or tails.
+        if random.randint(0, 1):
+            self.side_facing_up = "heads"
+        else:
+            self.side_facing_up = "tails"
 
 
 # Create an instance of the Coin class. Flip the coin and print the result.
-
+# This creates us an instance of the Coin class.
+coin = Coin()
+# We use the flip function to "flip" the coin.
+coin.flip()
+# We print the side that is currently face up.
+print(coin.side_facing_up)
 
 # Let's flip the coin 10 times. Printing the result each time.
+# Let's use a for loop to ensure we don't have to write the same code over and over again.
+# The range function gives us a list of numbers to iterate through.
+for x in range(10):
+    coin.flip()
+    # Here we can use Python's f-strings to make the string formatting nicer.
+    print(f"Flip #{x}: {coin.side_facing_up}")
 
 
 # 3 - Inheritance.
@@ -35,7 +66,7 @@ import random
 # the child class.
 
 # Create another instance of "Coin", specifying a value of 0.05.
-
+coin = Coin(0.05)
 
 # In the UK, this would be a perfectly valid value for a coin to have, but in New Zealand,
 # where the smallest denomination is 0.10, this isn't the case.
@@ -44,14 +75,41 @@ import random
 # but have some changes specific to where the coin is from.
 
 # Let's create a new class for New Zealand coins, which inherits from the Coin class.
+class NewZealandCoin(Coin):
+    # We define a list of allowed values that the coin can have.
+    allowed_values = [0.10, 0.20, 0.50, 1.0, 2.0]
+
+    # We use the __init__() function again, using a default value of 1.0.
+    def __init__(self, value = 1.0):
+        # When initialising child classes, you can use Python's super() function to inherit the
+        # methods and properties from the parent class.
+        super().__init__(value)
+
+        # In addition to this, we want to check that the specified value is valid,
+        # raising an Exception if not.
+        # Notice that we are able to extract
+        if self.value not in self.allowed_values:
+            raise Exception(f"Specified value of coin is not in allowed list: {self.value}")
+
+    # We can add new methods to this child class.
+    def print_description(self):
+        # Notice that this string is split over multiple lines. Python code often sticks to a
+        # maximum line length. I've tried to stick to a maximum length of 100 characters.
+        print(
+            f"This is a New Zealand coin with value {self.value}, "
+            f"and {self.side_facing_up} facing up"
+        )
 
 
 # Let's try this new class out.
 # Creating an object with a value of 0.05 should throw an exception.
-
+# Uncomment the line below to see this for yourself.
+# nz_coin = NewZealandCoin(0.05)
 # Creating a coin with a valid value works nicely.
-
+nz_coin = NewZealandCoin(0.5)
 # Let's flip the coin and print a description.
+nz_coin.flip()
+nz_coin.print_description()
 
 
 # This has barely scratched the surface of what you can do with Python classes. I would encourage
--- 3-real-world-example/real_world_example.py
+++ 3-real-world-example/real_world_example.py
@@ -45,3 +45,48 @@ expected_config = {
 
 
 # Write a method to remove the elements that we don't want.
+# We write this function to work recursively.
+def clean_config(config):
+    # The first thing we want to do is check for the key in the dict that we've been passed.
+    if "present" in config:
+        # The "present" key is present in this config dict. If the corresponding value is false,
+        # then we shouldn't include this particular dict, and return None instead.
+        # We purposefully return None, because an empty dict is valid config that we could want.
+        if not config["present"]:
+            return None
+
+    # We can now proceed.
+    # We loop through the given config dict and construct a corresponding output dict.
+    # We either recursively run over nested dicts, or write corresponding key/value pairs
+    # in the output dict.
+    updated_config = {}
+    for key, value in config.items():
+        if isinstance(value, dict):
+            # If value is a dict, then we want to remove any "present: false" elements.
+            value = clean_config(value)
+            # If the returned value is None, then the original dict contained the "present: false"
+            # flag, and so shouldn't be included in the output.
+            if value is not None:
+                updated_config[key] = value
+
+        else:
+            # The value isn't a dict, so we put into the output dict unaltered.
+            updated_config[key] = value
+
+    # Finally, we can return the updated config that we have constructed.
+    return updated_config
+
+
+# Let's test this function.
+cleaned_config = clean_config(config_example)
+
+# Use the json module to produce strings that we can compare.
+# This is a "trick" to compare two dictionaries, which can be a difficult thing to do in Python.
+import json
+cleaned_config_str = json.dumps(cleaned_config, indent=4, sort_keys=True)
+expected_config_str = json.dumps(expected_config, indent=4, sort_keys=True)
+
+if cleaned_config_str == expected_config_str:
+    print("The two dicts are equal")
+else:
+    print("The two dicts are different")
